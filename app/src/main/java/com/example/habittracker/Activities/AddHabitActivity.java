5a6974e35a2dd0aa4193d1f75980f0931f602444
root <root@LAPTOP-QICT8CL7.localdomain>
2024-12-16 20:19:39 +0530

    Added new components 1>EditText View 2>Done Button
    OnPress of add button is working

--- app/src/main/java/com/example/habittracker/Activities/AddHabitActivity.java
+++ app/src/main/java/com/example/habittracker/Activities/AddHabitActivity.java
@@ -1,6 +1,10 @@
 package com.example.habittracker.Activities;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,17 +16,38 @@ import com.example.habittracker.R;
 
 public class AddHabitActivity extends AppCompatActivity {
 
+    private Button add_button;
+    private EditText editText;
+    private Button done_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_habit);
-
-
+        initViews();
+        handleAdd();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void initViews(){
+        add_button=(Button) findViewById(R.id.Add);
+        editText=(EditText) findViewById(R.id.editTextText);
+        done_button=(Button) findViewById(R.id.Done);
+    }
+
+    private void handleAdd(){
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_button.setVisibility(View.GONE);
+                editText.setVisibility(View.VISIBLE);
+                done_button.setVisibility(View.VISIBLE);
+            }
         });
     }
 }
